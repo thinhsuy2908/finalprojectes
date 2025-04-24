503526e46768fdee1e9592cc0fdb6cefbd08eff0
thinhsuy2908 <phan90539@gmail.com>
2025-04-24 09:02:11 +0700

    buoi 1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// let users = localStorage.getItem("users")
+// let currentUser = JSON.parse(localStorage.getItem("currentUser"))
+
+// location.href = "./home.html"
+
+// const content = document.querySelector(".content")
+
+// content.innerHTML = `<h2 class="fw-bold mb-3">Chào mừng bạn đã quay lại học, <span id="userDisplay">${currentUser}</span>!</h2>`
